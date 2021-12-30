6678e1f388d15b404e16098d1b8fc057e070a941
Jim <ecksi@Jims-MacBook-Pro.local>
2021-12-29 22:03:13 -0500

    Prettier code

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,27 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
-const port = 5000;
+let port = process.env.PORT;
+
+if (port == null || port == "") {
+  port = "5000";
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-requested-With, Content-Type, Accept"
+  );
+});
 
-app.get(
-  '/',
-  async (req: Request, res: Response): Promise<Response> => {
-    return res.status(200).send({
-      message: "Hello",
-    });
-  }
-);
+app.get("/", async (req: Request, res: Response): Promise<Response> => {
+  return res.status(200).send({
+    message: "Hello",
+  });
+});
 
 try {
   app.listen(port, (): void => {
